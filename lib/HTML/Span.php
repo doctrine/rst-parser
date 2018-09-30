d53341b95e4016ea58e5ab3332008f596e5679c8
Nicolas PHILIPPE <nikophil@gmail.com>
2018-09-30 17:13:08 +0200

    add $attributes to ResolvedReference

--- lib/HTML/Span.php
+++ lib/HTML/Span.php
@@ -6,7 +6,11 @@ namespace Doctrine\RST\HTML;
 
 use Doctrine\RST\References\ResolvedReference;
 use Doctrine\RST\Span as Base;
+use function array_keys;
+use function array_map;
 use function htmlspecialchars;
+use function implode;
+use function sprintf;
 use function trim;
 
 class Span extends Base
@@ -36,9 +40,23 @@ class Span extends Base
         return '<code>' . $text . '</code>';
     }
 
-    public function link(?string $url, string $title) : string
+    /**
+     * @param string[] $attributes
+     */
+    public function link(?string $url, string $title, array $attributes = []) : string
     {
-        return '<a href="' . htmlspecialchars((string) $url) . '">' . $title . '</a>';
+        $htmlAttributes = implode(
+            ' ',
+            array_map(
+                static function ($attribute, $value) {
+                    return sprintf('%s="%s"', $attribute, htmlspecialchars((string) $value));
+                },
+                array_keys($attributes),
+                $attributes
+            )
+        );
+
+        return '<a href="' . htmlspecialchars((string) $url) . '"' . ( $htmlAttributes !== '' ? ' ' . $htmlAttributes : '') . '>' . $title . '</a>';
     }
 
     public function escape(string $span) : string
@@ -61,15 +79,15 @@ class Span extends Base
             if ($value['anchor'] !== null) {
                 $url .= '#' . $value['anchor'];
             }
-            $link = $this->link($url, $text);
+            $link = $this->link($url, $text, $reference->getAttributes());
 
         // reference to anchor in existing document
         } elseif ($value['url'] !== null) {
             $url = $this->environment->getLink($value['url']);
 
-            $link = $this->link($url, $text);
+            $link = $this->link($url, $text, $reference->getAttributes());
         } else {
-            $link = $this->link('#', $text . ' (unresolved reference)');
+            $link = $this->link('#', $text . ' (unresolved reference)', $reference->getAttributes());
         }
 
         return $link;
