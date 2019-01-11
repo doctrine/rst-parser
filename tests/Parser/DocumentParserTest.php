54bf9de102c1988055326a16643ae5abad3cd89d
Ryan Weaver <ryan@thatsquality.com>
2019-01-11 14:21:45 -0500

    Allowing directives to throw an exception and have a decent error

---
+++ tests/Parser/DocumentParserTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Tests\RST\Parser;
+
+use Doctrine\Common\EventManager;
+use Doctrine\RST\Directives\Directive;
+use Doctrine\RST\Environment;
+use Doctrine\RST\NodeFactory\NodeFactory;
+use Doctrine\RST\Parser;
+use Doctrine\RST\Parser\DocumentParser;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class DocumentParserTest extends TestCase
+{
+    public function testErrorWhenDirectiveThrowsException() : void
+    {
+        $parser             = $this->createMock(Parser::class);
+        $environment        = $this->createMock(Environment::class);
+        $nodeFactory        = $this->createMock(NodeFactory::class);
+        $eventManager       = $this->createMock(EventManager::class);
+        $codeBlockDirective = $this->createMock(Directive::class);
+
+        $docParser = new DocumentParser(
+            $parser,
+            $environment,
+            $nodeFactory,
+            $eventManager,
+            ['code-block' => $codeBlockDirective],
+            true,
+            __DIR__
+        );
+
+        $codeBlockDirective->expects($this->once())
+            ->method('process')
+            ->willThrowException(new Exception('Invalid something something!'));
+        $codeBlockDirective->expects($this->once())
+            ->method('getName')
+            ->willReturn('code-block-name');
+
+        $environment->expects($this->once())
+            ->method('addError')
+            ->with('Error while processing "code-block-name" directive: Invalid something something!');
+
+        $docParser->parse('.. code-block:: php');
+    }
+}
