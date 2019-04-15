bdced867ee82fbe4ee049f5b0043bdfe5add0527
Jonathan H. Wage <jonwage@gmail.com>
2019-04-15 13:47:24 -0500

    Add assertions to BuilderMalformedReferenceTest.

--- tests/BuilderMalformedReference/BuilderMalformedReferenceTest.php
+++ tests/BuilderMalformedReference/BuilderMalformedReferenceTest.php
@@ -20,13 +20,24 @@ class BuilderMalformedReferenceTest extends BaseBuilderTest
         $this->configuration = new Configuration();
         $this->configuration->setUseCachedMetas(false);
         $this->configuration->abortOnError(false);
+        $this->configuration->setIgnoreInvalidReferences(true);
 
         $this->builder = new Builder(new Kernel($this->configuration));
     }
 
     public function testMalformedReference() : void
     {
+        // test that invalid references can be ignored and no exception gets thrown
+
         $this->builder->build($this->sourceFile(), $this->targetFile());
+
+        $contents = $this->getFileContents($this->targetFile('subdir/another.html'));
+
+        self::assertContains('<p>Test link to</p>', $contents);
+
+        $contents = $this->getFileContents($this->targetFile('subdir/index.html'));
+
+        self::assertContains('<a id="test_reference"></a>', $contents);
     }
 
     protected function getFixturesDirectory() : string
