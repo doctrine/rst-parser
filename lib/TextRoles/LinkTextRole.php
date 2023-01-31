f85c33cf3000100cc04e1c06ae0dee7934c2d9e3
lina.wolf <lwolf@w-commerce.de>
2023-01-31 21:58:49 +0000

    [TASK] Delegate link rendering to text-role

--- lib/TextRoles/LinkTextRole.php
+++ lib/TextRoles/LinkTextRole.php
@@ -4,12 +4,54 @@ declare(strict_types=1);
 
 namespace Doctrine\RST\TextRoles;
 
+use Doctrine\RST\Environment;
+use Doctrine\RST\InvalidLink;
 use Doctrine\RST\Span\SpanToken;
 
-class LinkTextRole extends WrapperTextRole
+class LinkTextRole extends BaseTextRole
 {
-    public function __construct()
+    public function getName(): string
     {
-        parent::__construct(SpanToken::TYPE_LINK);
+        return SpanToken::TYPE_LINK;
+    }
+
+    public function render(Environment $environment, SpanToken $spanToken): string
+    {
+        $url      = $spanToken->get('url');
+        $link     = $spanToken->get('link');
+        $linktext = $spanToken->get('linktext');
+
+        if ($url === '') {
+            $linkTarget = $environment->getLinkTarget($link);
+            if ($linkTarget !== null) {
+                $url = $linkTarget->getUrl();
+            }
+
+            if ($url === '') {
+                $metaEntry = $environment->getMetaEntry();
+
+                if ($metaEntry !== null && $metaEntry->hasTitle($link)) {
+                    // A strangely-complex way to simply get the current relative URL
+                    // For example, if the current page is "reference/page", then
+                    // this would return "page" so the final URL is href="page#some-anchor".
+                    $currentRelativeUrl = $environment->relativeUrl('/' . $metaEntry->getUrl());
+                    $url                = $currentRelativeUrl . '#' . Environment::slugify($link);
+                }
+            }
+
+            if ($url === '') {
+                $environment->addInvalidLink(new InvalidLink($link));
+
+                return $link;
+            }
+        }
+
+        return $this->link($environment, $url, $linktext);
+    }
+
+    /** @param mixed[] $attributes */
+    public function link(Environment $environment, ?string $url, string $title, array $attributes = []): string
+    {
+        return $environment->getLinkRenderer()->renderUrl($url, $title, $attributes);
     }
 }
